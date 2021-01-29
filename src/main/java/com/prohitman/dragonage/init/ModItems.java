90993907e64192cc0bbca78a9383b691a574958f
prohitman <43189387+prohitman@users.noreply.github.com>
2021-01-29 18:45:41 +0100

    it works?

--- src/main/java/com/prohitman/dragonage/init/ModItems.java
+++ src/main/java/com/prohitman/dragonage/init/ModItems.java
@@ -2,9 +2,7 @@ package com.prohitman.dragonage.init;
 
 import com.prohitman.dragonage.DragonsDungeons;
 import com.prohitman.dragonage.client.renderers.SteelShieldItemStackTileEntityRenderer;
-import com.prohitman.dragonage.items.HalberdItem;
-import com.prohitman.dragonage.items.ModItemTiers;
-import com.prohitman.dragonage.items.SteelShieldItem;
+import com.prohitman.dragonage.items.*;
 
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.HoeItem;
@@ -31,47 +29,47 @@ public class ModItems
 	public static final RegistryObject<Item> GILDED_IRON_INGOT = ITEMS.register("gilded_iron_ingot", () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS).maxStackSize(64)));
 
 	//Weapons
-	public static final RegistryObject<Item> STEEL_SWORD = ITEMS.register("steel_sword", () -> new SwordItem(ModItemTiers.STEEL, 4, -2.4F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_WEAPONS)));
+	public static final RegistryObject<Item> STEEL_SWORD = ITEMS.register("steel_sword", () -> new DDSwordItem(ModItemTiers.STEEL, 4, -2.4F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_WEAPONS)));
 	public static final RegistryObject<Item> GREAT_STEEL_SWORD = ITEMS.register("great_steel_sword", () -> new SwordItem(ModItemTiers.STEEL, 4, -3.0F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_WEAPONS)));
 	public static final RegistryObject<Item> STEEL_SHIELD = ITEMS.register("steel_shield", () -> new SteelShieldItem(new Item.Properties().maxDamage(500).group(ModItemGroups.DRAGON_AGE_WEAPONS).setISTER(() -> SteelShieldItemStackTileEntityRenderer::new)));
 	public static final RegistryObject<Item> STEEL_HALBERD = ITEMS.register("steel_halberd", () -> new HalberdItem(ModItemTiers.STEEL, 4, -3.0F, 9.0F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_WEAPONS)));
 	public static final RegistryObject<Item> STEEL_BATTLE_AXE = ITEMS.register("steel_battle_axe", () -> new AxeItem(ModItemTiers.STEEL, 8.0F, -3.1F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_WEAPONS)));
 
-	public static final RegistryObject<Item> MITHRIL_SWORD = ITEMS.register("mithril_sword", () -> new SwordItem(ModItemTiers.MITHRIL, 3, -2.4F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_WEAPONS)));
-	public static final RegistryObject<Item> DWARVEN_STEEL_SWORD = ITEMS.register("dwarven_steel_sword", () -> new SwordItem(ModItemTiers.DWARVEN_STEEL, 3, -2.4F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_WEAPONS)));
-	public static final RegistryObject<Item> GILDED_IRON_SWORD = ITEMS.register("gilded_iron_sword", () -> new SwordItem(ModItemTiers.GILDED_IRON, 3, -2.4F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_WEAPONS)));
-	public static final RegistryObject<Item> DRAGON_BONE_SWORD = ITEMS.register("dragon_bone_sword", () -> new SwordItem(ModItemTiers.DRAGON_BONE, 4, -2.4F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_WEAPONS)));
-	public static final RegistryObject<Item> ELVEN_BRASS_SWORD = ITEMS.register("elven_brass_sword", () -> new SwordItem(ModItemTiers.ELVEN_BRASS, 3, -2.4F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_WEAPONS)));
+	public static final RegistryObject<Item> MITHRIL_SWORD = ITEMS.register("mithril_sword", () -> new DDSwordItem(ModItemTiers.MITHRIL, 6, -2.4F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_WEAPONS)));
+	public static final RegistryObject<Item> DWARVEN_STEEL_SWORD = ITEMS.register("dwarven_steel_sword", () -> new DDSwordItem(ModItemTiers.DWARVEN_STEEL, 2, -2.4F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_WEAPONS)));
+	public static final RegistryObject<Item> GILDED_IRON_SWORD = ITEMS.register("gilded_iron_sword", () -> new DDSwordItem(ModItemTiers.GILDED_IRON, 3, -2.4F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_WEAPONS)));
+	public static final RegistryObject<Item> DRAGON_BONE_SWORD = ITEMS.register("dragon_bone_sword", () -> new DDSwordItem(ModItemTiers.DRAGON_BONE, 4, -2.4F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_WEAPONS)));
+	public static final RegistryObject<Item> ELVEN_BRASS_SWORD = ITEMS.register("elven_brass_sword", () -> new DDSwordItem(ModItemTiers.ELVEN_BRASS, 4.5F, -2.4F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_WEAPONS)));
 
 	//Tools
-	public static final RegistryObject<Item> STEEL_SHOVEL = ITEMS.register("steel_shovel", () -> new ShovelItem(ModItemTiers.STEEL, 1.5F, -3.0F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> STEEL_PICKAXE = ITEMS.register("steel_pickaxe", () -> new PickaxeItem(ModItemTiers.STEEL, 1.5F, -2.8F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> STEEL_AXE = ITEMS.register("steel_axe", () -> new AxeItem(ModItemTiers.STEEL, 6.0F, -3.1F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> STEEL_HOE = ITEMS.register("steel_hoe", () -> new HoeItem(ModItemTiers.STEEL, -2, -0.9F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> STEEL_SHOVEL = ITEMS.register("steel_shovel", () -> new DDShovelItem(ModItemTiers.STEEL, 1.5F, -3.0F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> STEEL_PICKAXE = ITEMS.register("steel_pickaxe", () -> new DDPickaxeItem(ModItemTiers.STEEL, 1.5F, -2.8F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> STEEL_AXE = ITEMS.register("steel_axe", () -> new DDAxeItem(ModItemTiers.STEEL, 6.5F, -3.1F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> STEEL_HOE = ITEMS.register("steel_hoe", () -> new DDHoeItem(ModItemTiers.STEEL, 0.5f, -0.9F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
 
-	public static final RegistryObject<Item> MITHRIL_SHOVEL = ITEMS.register("mithril_shovel", () -> new ShovelItem(ModItemTiers.MITHRIL, 1.5F, -3.0F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> MITHRIL_PICKAXE = ITEMS.register("mithril_pickaxe", () -> new PickaxeItem(ModItemTiers.MITHRIL, 1, -2.8F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> MITHRIL_AXE = ITEMS.register("mithril_axe", () -> new AxeItem(ModItemTiers.MITHRIL, 6.0F, -3.1F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> MITHRIL_HOE = ITEMS.register("mithril_hoe", () -> new HoeItem(ModItemTiers.MITHRIL, -2, -0.9F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> MITHRIL_SHOVEL = ITEMS.register("mithril_shovel", () -> new DDShovelItem(ModItemTiers.MITHRIL, 2.0F, -3.0F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> MITHRIL_PICKAXE = ITEMS.register("mithril_pickaxe", () -> new DDPickaxeItem(ModItemTiers.MITHRIL, 3, -2.8F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> MITHRIL_AXE = ITEMS.register("mithril_axe", () -> new DDAxeItem(ModItemTiers.MITHRIL, 8.0F, -3.1F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> MITHRIL_HOE = ITEMS.register("mithril_hoe", () -> new DDHoeItem(ModItemTiers.MITHRIL, 1, -0.9F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
 
-	public static final RegistryObject<Item> DWARVEN_STEEL_SHOVEL = ITEMS.register("dwarven_steel_shovel", () -> new ShovelItem(ModItemTiers.DWARVEN_STEEL, 1.5F, -3.0F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> DWARVEN_STEEL_PICKAXE = ITEMS.register("dwarven_steel_pickaxe", () -> new PickaxeItem(ModItemTiers.DWARVEN_STEEL, 1, -2.8F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> DWARVEN_STEEL_AXE = ITEMS.register("dwarven_steel_axe", () -> new AxeItem(ModItemTiers.DWARVEN_STEEL, 6.0F, -3.1F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> DWARVEN_STEEL_HOE = ITEMS.register("dwarven_steel_hoe", () -> new HoeItem(ModItemTiers.DWARVEN_STEEL, -2, -0.9F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> DWARVEN_STEEL_SHOVEL = ITEMS.register("dwarven_steel_shovel", () -> new DDShovelItem(ModItemTiers.DWARVEN_STEEL, -2F, -3.0F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> DWARVEN_STEEL_PICKAXE = ITEMS.register("dwarven_steel_pickaxe", () -> new DDPickaxeItem(ModItemTiers.DWARVEN_STEEL, -2, -2.8F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> DWARVEN_STEEL_AXE = ITEMS.register("dwarven_steel_axe", () -> new DDAxeItem(ModItemTiers.DWARVEN_STEEL, 2.0F, -3.1F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> DWARVEN_STEEL_HOE = ITEMS.register("dwarven_steel_hoe", () -> new DDHoeItem(ModItemTiers.DWARVEN_STEEL, -3, -0.9F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
 
-	public static final RegistryObject<Item> GILDED_IRON_SHOVEL = ITEMS.register("gilded_iron_shovel", () -> new ShovelItem(ModItemTiers.GILDED_IRON, 1.5F, -3.0F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> GILDED_IRON_PICKAXE = ITEMS.register("gilded_iron_pickaxe", () -> new PickaxeItem(ModItemTiers.GILDED_IRON, 1, -2.8F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> GILDED_IRON_AXE = ITEMS.register("gilded_iron_axe", () -> new AxeItem(ModItemTiers.GILDED_IRON, 6.0F, -3.1F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> GILDED_IRON_HOE = ITEMS.register("gilded_iron_hoe", () -> new HoeItem(ModItemTiers.GILDED_IRON, -2, -0.9F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> GILDED_IRON_SHOVEL = ITEMS.register("gilded_iron_shovel", () -> new DDShovelItem(ModItemTiers.GILDED_IRON, 1.5F, -3.0F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> GILDED_IRON_PICKAXE = ITEMS.register("gilded_iron_pickaxe", () -> new DDPickaxeItem(ModItemTiers.GILDED_IRON, 1, -2.8F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> GILDED_IRON_AXE = ITEMS.register("gilded_iron_axe", () -> new DDAxeItem(ModItemTiers.GILDED_IRON, 6.0F, -3.1F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> GILDED_IRON_HOE = ITEMS.register("gilded_iron_hoe", () -> new DDHoeItem(ModItemTiers.GILDED_IRON, -2, -0.9F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
 
-	public static final RegistryObject<Item> DRAGON_BONE_SHOVEL = ITEMS.register("dragon_bone_shovel", () -> new ShovelItem(ModItemTiers.DRAGON_BONE, 1.5F, -3.0F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> DRAGON_BONE_PICKAXE = ITEMS.register("dragon_bone_pickaxe", () -> new PickaxeItem(ModItemTiers.DRAGON_BONE, 0, -2.8F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> DRAGON_BONE_AXE = ITEMS.register("dragon_bone_axe", () -> new AxeItem(ModItemTiers.DRAGON_BONE, 6.0F, -3.1F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> DRAGON_BONE_HOE = ITEMS.register("dragon_bone_hoe", () -> new HoeItem(ModItemTiers.DRAGON_BONE, -1, -0.9F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> DRAGON_BONE_SHOVEL = ITEMS.register("dragon_bone_shovel", () -> new DDShovelItem(ModItemTiers.DRAGON_BONE, 0F, -3.0F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> DRAGON_BONE_PICKAXE = ITEMS.register("dragon_bone_pickaxe", () -> new DDPickaxeItem(ModItemTiers.DRAGON_BONE, 0, -2.8F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> DRAGON_BONE_AXE = ITEMS.register("dragon_bone_axe", () -> new DDAxeItem(ModItemTiers.DRAGON_BONE, 6.0F, -3.1F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> DRAGON_BONE_HOE = ITEMS.register("dragon_bone_hoe", () -> new DDHoeItem(ModItemTiers.DRAGON_BONE, -1, -0.9F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
 
-	public static final RegistryObject<Item> ELVEN_BRASS_SHOVEL = ITEMS.register("elven_brass_shovel", () -> new ShovelItem(ModItemTiers.ELVEN_BRASS, 1.5F, -3.0F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> ELVEN_BRASS_PICKAXE = ITEMS.register("elven_brass_pickaxe", () -> new PickaxeItem(ModItemTiers.ELVEN_BRASS, 1, -2.8F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> ELVEN_BRASS_AXE = ITEMS.register("elven_brass_axe", () -> new AxeItem(ModItemTiers.ELVEN_BRASS, 6.0F, -3.1F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
-	public static final RegistryObject<Item> ELVEN_BRASS_HOE = ITEMS.register("elven_brass_hoe", () -> new HoeItem(ModItemTiers.ELVEN_BRASS, -2, -0.9F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> ELVEN_BRASS_SHOVEL = ITEMS.register("elven_brass_shovel", () -> new DDShovelItem(ModItemTiers.ELVEN_BRASS, 0F, -3.0F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> ELVEN_BRASS_PICKAXE = ITEMS.register("elven_brass_pickaxe", () -> new DDPickaxeItem(ModItemTiers.ELVEN_BRASS, 1, -2.8F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> ELVEN_BRASS_AXE = ITEMS.register("elven_brass_axe", () -> new DDAxeItem(ModItemTiers.ELVEN_BRASS, 5.0F, -3.1F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
+	public static final RegistryObject<Item> ELVEN_BRASS_HOE = ITEMS.register("elven_brass_hoe", () -> new DDHoeItem(ModItemTiers.ELVEN_BRASS, -1F, -0.9F, new Item.Properties().group(ModItemGroups.DRAGON_AGE_TOOLS)));
 
 }
--- src/main/java/com/prohitman/dragonage/items/ModItemTiers.java
+++ src/main/java/com/prohitman/dragonage/items/ModItemTiers.java
@@ -1,90 +1,77 @@
 package com.prohitman.dragonage.items;
 
-import java.util.function.Supplier;
-
 import com.prohitman.dragonage.init.ModItems;
-
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.LazyValue;
 
-public enum ModItemTiers implements IItemTier
-{
-	STEEL(2, 800, 6.0F, 2.5F, 12, () ->
-	{
-		return Ingredient.fromItems(ModItems.STEEL_INGOT.get());
-	}),
+import java.util.function.Supplier;
 
-	MITHRIL(4, 1800, 9.0F, 2.5F, 12, () ->
-	{
-		return Ingredient.fromItems(ModItems.MITHRIL_INGOT.get());
-	}),
+public enum ModItemTiers implements IItemTier {
+    STEEL(2, 800, 6.0F, 2.5F, 12, () -> {
+        return Ingredient.fromItems(ModItems.STEEL_INGOT.get());
+    }),
+    MITHRIL(4, 1800, 9.0F, 3F, 12, () -> {
+        return Ingredient.fromItems(ModItems.MITHRIL_INGOT.get());
+    }),
+    ELVEN_BRASS(3, 910, 8.0F, 4.0F, 14, () -> {
+        return Ingredient.fromItems(ModItems.ELVEN_BRASS_INGOT.get());
+    }),
+    DRAGON_BONE(5, 3000, 10.0F, 6.0F, 16, () -> {
+        return Ingredient.fromItems(ModItems.DRAGON_BONE.get());
+    }),
+    DWARVEN_STEEL(3, 1400, 8.0F, 7F, 15, () -> {
+        return Ingredient.fromItems(ModItems.DWARVEN_STEEL_INGOT.get());
+    }),
+    GILDED_IRON(2, 275, 6.0F, 2.0F, 18, () -> {
+        return Ingredient.fromItems(ModItems.GILDED_IRON_INGOT.get());
+    });
 
-	ELVEN_BRASS(3, 910, 8.0F, 2.5F, 14, () ->
-	{
-		return Ingredient.fromItems(ModItems.ELVEN_BRASS_INGOT.get());
-	}),
 
-	DRAGON_BONE(5, 3000, 10.0F, 6.0F, 16, () ->
-	{
-		return Ingredient.fromItems(ModItems.DRAGON_BONE.get());
-	}),
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamage;
+    private final int enchantability;
+    private final LazyValue<Ingredient> repairMaterial;
 
-	DWARVEN_STEEL(3, 1400, 8.0F, 2.5F, 15, () ->
-	{
-		return Ingredient.fromItems(ModItems.DWARVEN_STEEL_INGOT.get());
-	}),
+    private ModItemTiers(int harvestLevelIn, int maxUsesIn, float efficiencyIn, float attackDamageIn, int enchantabilityIn, Supplier<Ingredient> repairMaterialIn) {
+        this.harvestLevel = harvestLevelIn;
+        this.maxUses = maxUsesIn;
+        this.efficiency = efficiencyIn;
+        this.attackDamage = attackDamageIn;
+        this.enchantability = enchantabilityIn;
+        this.repairMaterial = new LazyValue<>(repairMaterialIn);
+    }
 
-	GILDED_IRON(2, 275, 6.0F, 2.5F, 18, () ->
-	{
-		return Ingredient.fromItems(ModItems.GILDED_IRON_INGOT.get());
-	});
-	
-	
-	private final int harvestLevel;
-	private final int maxUses;
-	private final float efficiency;
-	private final float attackDamage;
-	private final int enchantability;
-	private final LazyValue<Ingredient> repairMaterial;
+    @Override
+    public int getMaxUses() {
+        return this.maxUses;
+    }
 
-	private ModItemTiers(int harvestLevelIn, int maxUsesIn, float efficiencyIn, float attackDamageIn, int enchantabilityIn, Supplier<Ingredient> repairMaterialIn) {
-		this.harvestLevel = harvestLevelIn;
-		this.maxUses = maxUsesIn;
-		this.efficiency = efficiencyIn;
-		this.attackDamage = attackDamageIn;
-		this.enchantability = enchantabilityIn;
-		this.repairMaterial = new LazyValue<>(repairMaterialIn);
-	}	
-	
-	@Override
-	public int getMaxUses() {
-		return this.maxUses;
-	}
+    @Override
+    public float getEfficiency() {
+        return this.efficiency;
+    }
 
-	@Override
-	public float getEfficiency() {
-		return this.efficiency;
-	}
+    @Override
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
 
-	@Override
-	public float getAttackDamage() {
-		return this.attackDamage;
-	}
+    @Override
+    public int getHarvestLevel() {
+        return this.harvestLevel;
+    }
 
-	@Override
-	public int getHarvestLevel() {
-		return this.harvestLevel;
-	}
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
 
-	@Override
-	public int getEnchantability() {
-		return this.enchantability;
-	}
+    @Override
+    public Ingredient getRepairMaterial() {
+        return this.repairMaterial.getValue();
+    }
 
-	@Override
-	public Ingredient getRepairMaterial() {
-		return this.repairMaterial.getValue();
-	}
-	
 }
